2d95b18cdfd953401635ade0501c4f07e42e9ef8
Gguife <gguife747@gmail.com>
2023-12-01 15:33:12 -0300

    feat: Creating menu nav

--- src/components/header/Index.tsx
+++ src/components/header/Index.tsx
@@ -1,9 +1,33 @@
 import "./Style.css";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () =>{
+  const location = useLocation();
+  const [activePage, setActivePage] = useState("");
+
+  useEffect(() => {
+    setActivePage(location.pathname);
+  }, [location.pathname]);
+
   return (
     <header>
-      testando Menu
+      <h1>Naped</h1>
+      <div className="container-menu">
+        <nav>
+          <ul>
+            <li className={activePage === "/" ? "menu-item active" : "menu-item"}><Link to="/">Home</Link></li>
+            <li className={activePage === "/series" ? "menu-item active" : "menu-item"}><Link to="/series">Séries</Link></li>
+            <li className={activePage === "/filmes" ? "menu-item active" : "menu-item"}><Link to="/filmes">Filmes</Link></li>
+            <li className={activePage === "/animes" ? "menu-item active" : "menu-item"}><Link to="/animes">Animes</Link></li>
+            <li className={activePage === "/games" ? "menu-item active" : "menu-item"}><Link to="/games">Games</Link></li>
+          </ul>
+        </nav>
+        <button>Minha conta</button>
+      </div>
+      <div className="menu-mobile">
+
+      </div>
     </header>
   )
 }
