34e3accbd95cbdc7b1e89bf02bc8920a1f481ae4
Gguife <gguife747@gmail.com>
2023-12-01 15:32:15 -0300

    feat: Router structure completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header/Index'
+import { MainRouter } from './router/MainRouter'
 
 function App() {
 
   return (
     <>
       <Header />
+      <MainRouter />
     </>
   )
 }
