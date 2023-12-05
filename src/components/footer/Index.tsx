37a38a5126c8325d35b07c419153b81daa2569a2
Gguife <gguife747@gmail.com>
2023-12-05 18:15:54 -0300

    feat: Finished footer

---
+++ src/components/footer/Index.tsx
@@ -0,0 +1,12 @@
+import "./Style.css";
+
+const Footer = () =>{
+  return (
+    <footer>
+      <h1>Naped</h1>
+      <p>Todas as imagens de filmes, séries e etc são marcas registradas dos seus respectivos proprietários.</p>
+    </footer>
+  )
+}
+
+export default Footer;
