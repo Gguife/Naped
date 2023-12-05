395b3088032fdd6306a1de229eb8f7b592194e09
Gguife <gguife747@gmail.com>
2023-12-05 18:05:07 -0300

    feat: Completed Page Home

--- src/View/home/Index.tsx
+++ src/View/home/Index.tsx
@@ -1,17 +1,21 @@
 import NoScrollCard from "../../components/home/NoScrollCards/Index";
 import Hero from "../../components/home/hero/Index";
+import NewsHome from "../../components/home/newsHome/Index";
 import ScrollCards from "../../components/home/scrollCards/Index";
 import "./Style.css";
 
 const Home = () => {
   return (
     <div className="home">
-        <section className="hero">
+        <section className="hero section">
           <Hero />
         </section>
-        <section className="section-scroll">
+        <section className="section-scroll section">
           <ScrollCards />
           <NoScrollCard />
+        </section>
+        <section className="section-news">
+          <NewsHome />
         </section>
     </div>
   )
