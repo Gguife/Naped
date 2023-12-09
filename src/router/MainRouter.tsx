5d573170b80e50577b458eae8d559d3c97430d14
Gguife <gguife747@gmail.com>
2023-12-09 00:01:20 -0300

    feat: Added series route

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -3,11 +3,13 @@ import { useRoutes } from "react-router-dom";
 //Views
 import Home from "../View/home/Index"
 import News from "../View/news/Index";
+import Series from "../View/Series/Index";
 
 export const MainRouter = () =>{
   const router = useRoutes([
     {path: "/", element: <Home />},
     {path: "/noticia", element: <News />},
+    {path: "/series", element: <Series />}
   ])
 
   return router;
