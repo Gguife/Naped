19e3ac81d570531032bf92ec00d8c2a1c5f81f58
Gguife <gguife747@gmail.com>
2023-12-09 16:08:38 -0300

    feat: Added movies page route

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -4,12 +4,14 @@ import { useRoutes } from "react-router-dom";
 import Home from "../View/home/Index"
 import News from "../View/news/Index";
 import Series from "../View/Series/Index";
+import Movies from "../View/movies/Index";
 
 export const MainRouter = () =>{
   const router = useRoutes([
     {path: "/", element: <Home />},
     {path: "/noticia", element: <News />},
-    {path: "/series", element: <Series />}
+    {path: "/series", element: <Series />},
+    {path: "/filmes", element: <Movies />}
   ])
 
   return router;
