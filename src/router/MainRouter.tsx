f688d17c959e49e7dd0c875f426807bc2796a3d7
Gguife <gguife747@gmail.com>
2023-12-09 17:04:42 -0300

    feat: Added 404 route

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -7,6 +7,7 @@ import Series from "../View/Series/Index";
 import Movies from "../View/movies/Index";
 import Animes from "../View/Animes/Index";
 import Games from "../View/Games/Index";
+import NotFound from "../View/404/Index";
 
 export const MainRouter = () =>{
   const router = useRoutes([
@@ -15,7 +16,8 @@ export const MainRouter = () =>{
     {path: "/series", element: <Series />},
     {path: "/filmes", element: <Movies />},
     {path: "/animes", element: <Animes />},
-    {path: "/games", element: <Games />}
+    {path: "/games", element: <Games />},
+    {path: "*", element: <NotFound />}
   ])
 
   return router;
