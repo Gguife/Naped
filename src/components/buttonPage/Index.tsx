00411c84031a712f2c7a250ab00985f05ab18c23
Gguife <gguife747@gmail.com>
2023-12-09 00:00:13 -0300

    feat: Created Button component for Pages

---
+++ src/components/buttonPage/Index.tsx
@@ -0,0 +1,17 @@
+import "./Style.css";
+
+const ButtonPages = () => {
+  return (
+    <div className="buttons-container">
+      <span>🡨</span>
+      <span>1</span>
+      <span>2</span>
+      <span>...</span>
+      <span>9</span>
+      <span>10</span>
+      <span>🡪</span>
+    </div>
+  )
+}
+
+export default ButtonPages;
